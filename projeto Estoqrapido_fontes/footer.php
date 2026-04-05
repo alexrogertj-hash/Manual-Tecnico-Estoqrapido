b4a8a3e466ee50893bbc13180a9048f49f2e4d16
alexrogertj <alexroger.tj@gmail.com>
2026-04-05 19:06:04 -0300

    Delete projeto Estoqrapido_fontes directory

--- projeto Estoqrapido_fontes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer class="text-center mt-5 py-4 text-muted small">
-    &copy; <?= date('Y') ?> - EstoqRápido
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- projeto Estoqrapido_fontes/gerar_hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-$senha = '123';
-$hash = password_hash($senha, PASSWORD_BCRYPT);
-echo "Hash gerado para a senha '123':<br>" . $hash;
